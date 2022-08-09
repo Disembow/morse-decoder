a76d1efc71d82ab44d269fda49ece12db3f3b036
Disembow <e_naletov@mail.ru>
2022-08-09 15:15:51 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrNum = [];
+    for (i = 0; i < expr.length / 10; i++) {
+        arrNum.push(expr.slice(i * 10, (i + 1) * 10));
+    }
+    
+    let arrMorse =[];
+    arrNum.forEach(el => {
+        let shortStr = el.slice(el.indexOf('1', 0));
+        let morseStr = '';
+        for (i = 0; i < (shortStr.length / 2); i++) {
+            if (shortStr.slice(i * 2, (i + 1) * 2) === "*") {
+                morseStr += ' '
+            } else if (shortStr.slice(i * 2, (i + 1) * 2) === '10') {
+                morseStr += '.';
+            } else {
+                morseStr += '-';
+            }
+        }
+        arrMorse.push(morseStr);
+    })
+    let ArrWord = [];
+    arrMorse.forEach(el => {
+        if (MORSE_TABLE[el] === undefined) {
+            ArrWord.push(' ');
+        }
+        ArrWord.push(MORSE_TABLE[el]);
+    })
+    return ArrWord.join('');    
 }
 
 module.exports = {
