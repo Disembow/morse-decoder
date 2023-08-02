62b62910db440fba4e2a0e010462aa65dce89350
Disembow <e_naletov@mail.ru>
2023-08-02 12:41:48 +0200

    refactor: update code

--- src/index.js
+++ src/index.js
@@ -1,73 +1,75 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    let arrNum = [];
-    for (i = 0; i < expr.length / 10; i++) {
-        arrNum.push(expr.slice(i * 10, (i + 1) * 10));
+  let arrNum = [];
+
+  for (i = 0; i < expr.length / 10; i++) {
+    arrNum.push(expr.slice(i * 10, (i + 1) * 10));
+  }
+
+  let arrMorse = [];
+
+  arrNum.forEach((e) => {
+    let shortStr = e.slice(e.indexOf('1', 0));
+    let morseStr = '';
+
+    for (i = 0; i < shortStr.length / 2; i++) {
+      if (shortStr.slice(i * 2, (i + 1) * 2) === '*') {
+        morseStr += ' ';
+      } else if (shortStr.slice(i * 2, (i + 1) * 2) === '10') {
+        morseStr += '.';
+      } else {
+        morseStr += '-';
+      }
     }
-    
-    let arrMorse =[];
-    arrNum.forEach(el => {
-        let shortStr = el.slice(el.indexOf('1', 0));
-        let morseStr = '';
-        for (i = 0; i < (shortStr.length / 2); i++) {
-            if (shortStr.slice(i * 2, (i + 1) * 2) === "*") {
-                morseStr += ' '
-            } else if (shortStr.slice(i * 2, (i + 1) * 2) === '10') {
-                morseStr += '.';
-            } else {
-                morseStr += '-';
-            }
-        }
-        arrMorse.push(morseStr);
-    })
-    let ArrWord = [];
-    arrMorse.forEach(el => {
-        if (MORSE_TABLE[el] === undefined) {
-            ArrWord.push(' ');
-        }
-        ArrWord.push(MORSE_TABLE[el]);
-    })
-    return ArrWord.join('');    
+
+    arrMorse.push(morseStr);
+  });
+
+  let ArrWord = [];
+
+  arrMorse.forEach((e) => (!MORSE_TABLE[e] ? ArrWord.push(' ') : ArrWord.push(MORSE_TABLE[e])));
+
+  return ArrWord.join('');
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
